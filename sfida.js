036a5901453a2f3bcfe4c835e10ac56526a1fddf
Zgjim Topojani <65979294+pixelity2@users.noreply.github.com>
2022-12-29 18:56:33 +0100

    Add files via upload

---
+++ sfida.js
@@ -0,0 +1,14 @@
+function sfida(){
+var i=0;
+var array =["5",8,"4",];
+
+for (var i=0; i<array.length;i++){
+    
+}
+while(i%2==0){
+document.writeln("Nese vargu a dhe b jane union vargu b dhe c jane diference"+i);
+break;
+ }
+ 
+}
+sfida()
